f4df6e12ac222b4fba3139a8d92625e26e207c8f
Kourieh Edouard <malkekourie@hotmail.com>
2020-08-09 16:18:25 +0200

    function getAgo : done

--- StuckOverflow/Model/Post.cs
+++ StuckOverflow/Model/Post.cs
@@ -25,5 +25,65 @@ namespace prbd_1920_xyy
         {
             return $"{Title}";
         }
+
+        
+        public string GetAgo{
+            get
+            {
+                DateTime now = new DateTime();
+                now = DateTime.Now;
+
+                TimeSpan interval = now - DateTime;
+                string ret = "";
+                if (interval.TotalDays == 0)
+                {
+                    if (interval.TotalHours == 0)
+                    {
+                        if (interval.TotalMinutes == 0)
+                        {
+                            ret = interval.Seconds + " secondes ago ";
+                        }
+                        else
+                        {
+                            ret = interval.Minutes + " minutes ago ";
+                        }
+                    }
+                    else
+                    {
+                        ret = interval.Hours + " hours ago ";
+                    }
+                }
+                else
+                {
+                    if (interval.TotalDays >= 365)
+                    {
+                        int nbr = (int)interval.TotalDays / 365;
+                        ret = nbr + " years ago ";
+                    }
+                    else
+                    {
+                        if (interval.TotalDays < 30)
+                        {
+                            int nbr = (int)interval.TotalDays / 7;
+                            ret = nbr + " weeks ago ";
+
+                            if (interval.TotalDays < 7)
+                            {
+                                ret = interval.Days + " days ago ";
+                            }
+                        }
+                        else
+                        {
+                            int nbr = (int)interval.TotalDays / 30;
+                            ret = nbr + " months ago ";
+                        }
+                    }
+                }
+                return ret;
+
+            }
+        }
+
+
     }
 }
