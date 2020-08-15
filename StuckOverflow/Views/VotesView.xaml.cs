363ca9572bae7f5b2c290eb2025bb90a6d7f1e7d
Kourieh Edouard <malkekourie@hotmail.com>
2020-08-15 13:55:06 +0200

    voteView : sql not done

--- StuckOverflow/Views/VotesView.xaml.cs
+++ StuckOverflow/Views/VotesView.xaml.cs
@@ -14,35 +14,26 @@ namespace prbd_1920_xyy
         private ObservableCollection<Post> posts;
         public ObservableCollection<Post> Posts { get => posts; set => SetProperty(ref posts, value); }
 
-        //public ICommand DisplayMemberDetails { get; set; }
 
         public VotesView()
         {
             InitializeComponent();
 
             DataContext = this;
-            /*
-            DisplayMemberDetails = new RelayCommand<Member>(m => {
-                App.NotifyColleagues(AppMessages.MSG_DISPLAY_MEMBER, m);
-            });
-            */
+            
             Refresh();
         }
 
         private void Refresh()
         {
-            // Members = new ObservableCollection<Member>(App.Model.Members.OrderBy(m => m.Pseudo));
-            /*
-             * var q1 = from m in App.Model.Posts
-                     where m.Pseudo.Contains("a") || m.Profile.Contains("a")
-                     select m;
-             * 
-            */
+
+
             var q1 = from m in App.Model.Posts
                      where m.Title != null
                      orderby m.DateTime descending
                      select m;
             Posts = new ObservableCollection<Post>(q1);
+
         }
     }
 }
